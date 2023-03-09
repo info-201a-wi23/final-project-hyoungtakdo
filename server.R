59813b9d4991f59e6f4120a6c01a383eaf9bb229
hyoungtakdo <hdo3@uw.edu>
2023-03-08 17:17:26 -0800

    in progress work

--- server.R
+++ server.R
@@ -1,1 +1,97 @@
+library("dplyr")
+library("plotly")
 
+kidz_data <- read.csv("https://raw.githubusercontent.com/the-pudding/data/master/kidz-bop/KB_censored-lyrics.csv")
+
+KidzBop <- read.csv("https://raw.githubusercontent.com/the-pudding/data/master/kidz-bop/KB_word-overview.csv")
+
+sum_censored_words <- kidz_data %>% group_by(year) %>% 
+  summarise(count = sum(count))
+
+number_of_times_category <- kidz_data %>% group_by(category) %>%
+                                          summarise(count = sum(count))
+
+bad_words <- KidzBop %>% select(category, badword, inSongs, censored)
+                                          
+
+
+
+
+server <- function(input, output) {
+  
+  # This will be the output for the line chart of censored words per year
+  output$year_plot <- renderPlotly({
+    
+    year_data <- sum_censored_words %>% filter(year >= input$year_selection[1] &
+                                                 year <= input$year_selection[2])
+                              
+    
+    chart1_graph <- ggplot(data = year_data) +
+      geom_point(mapping = aes(
+        x = year,
+        y = count,
+        text = paste("\n Year:", year,
+                     "\n Censored Count:", count))) +
+      geom_line(mapping = aes(
+        x = year,
+        y = count)) +
+      
+      scale_y_continuous(labels = scales::comma) +
+      
+      labs(title = "Censored Words Per Year From 2001-2019",
+           x = "Year of Censorship",
+           y = "Censored Word Count") +
+      theme(plot.title = element_text(hjust = .5))
+    
+    return(ggplotly(chart1_graph, tooltip = c("text")))
+    
+  })
+  
+  # This will be the output for our category frequency chart
+  output$category_plot <- renderPlotly({
+    
+    category_data <- number_of_times_category %>% filter(category %in% input$category_frequency)
+     
+    category_plot <- ggplot(category_data) + 
+      geom_col(aes(x = category,
+                   y = count,
+                   fill = category,
+                   text = paste("\n Censored Category:", category,
+                                "\n Censored Count:", count))) + 
+      
+      labs(title = "Censored Category Frequency From 2001-2019", 
+           x = "Category", 
+           y = "The Frequency of a Category",
+           fill = "Category Name"
+           ) + 
+      theme(plot.title = element_text(hjust = .5)) +
+      scale_fill_brewer(palette = "Dark2") 
+    return(ggplotly(category_plot, tooltip = c("text")))
+    
+  })
+  
+  # This is the output for our Words VS Frequency Chart
+  output$frequency_plot <- renderPlotly({
+    
+    frequency_data <- bad_words %>% filter(badword %in% input$frequency_selection)
+    
+    frequency_plotted <- ggplot(data = frequency_data) +
+      geom_point(mapping = aes(
+        x = inSongs, 
+        y = censored, 
+        color = badword,
+        text = "\n Word:", badword,
+               "Original Count:", inSongs,
+               "Censored Count:", censored)) +
+      geom_smooth(mapping = aes(
+        x = inSongs, 
+        y = censored), 
+        method = "lm") +
+      labs(title = "How Often A Word Has Been Censored",
+           x = "Word Frequency",
+           y = "Censor Frequency")
+    
+    return(ggplotly(frequency_plotted, tooltip = c("text")))
+    
+  })
+}
--- ui.R
+++ ui.R
@@ -1,1 +1,102 @@
+library("shiny")
+library("tidyverse")
+library("plotly")
 
+intro_page <- tabPanel("Introduction",
+                       h1("Kidz Bop Censorship"))
+
+chart_1 <- tabPanel(
+  "Censored Words Per Year",
+   sidebarLayout(
+    sidebarPanel(
+      h3("Select Your Year Range:"),
+      sliderInput(inputId = "year_selection",
+                  label = "Years",
+                  min = 2001,
+                  max = 2019,
+                  value = c(2001, 2019),
+                  round = TRUE)),
+    mainPanel(
+      h1("Kidz Bop Censorship through the Years"),
+      plotlyOutput("year_plot"),
+      h3("Visualization Summary"),
+      p("The chart above gives us a line chart to represent and show
+         the audience the amount of words that were censored each year
+         from the data set of Kidz Bop Censorship. We decided to use a
+         line chart because it best shows the ups and downs of censorship
+         from year to year. We also decided to find the different amount
+         of censorship each year because we can try to find out if censorship
+         changes based on when different laws were passes and depended on the
+         time periods."),
+      p("This data set has revealed to us when the most and least amount of
+         censorship has happened in some of the years. For example, we found
+         that in 2015, Kidz Bop had the most censored words of any year while
+         2001 had the least. Findings like this from our data will
+         help us look closer into why this could be the case by seeing other
+         aspects of what happened in those years such as political, social,
+         and environmental events in other outside research."))
+      
+ )
+)
+  
+chart_2 <- tabPanel(
+  "Category Censorship",
+   sidebarLayout(
+     sidebarPanel(
+       h3("Category Frequency"),
+       selectInput(inputId = "category_frequency",
+                   label = "Categories of Censorship",
+                   choices = number_of_times_category$category,
+                   selected = c("profanity", "other"),
+                   multiple = TRUE)),
+     
+     mainPanel(
+       h1("Category Censorship Frequency"),
+       plotlyOutput("category_plot"),
+       h3("Visualization Summary"),
+       p("The bar chart above describes the number of times each category
+         was used as a reason for censorship in a song. This was an essential
+         visualization because the numbers can show which category the Kidz Bop
+         corporation prioritizes more regarding censorship. After creating this
+         visualization, the most common category used was profanity, which
+         wasn’t surprising because most songs contain profanity anyway."),
+       
+       p("The second most used category was sexual references, which surprised
+         us because we would think alcohol & drugs would be the second highest.
+         However, after overviewing the dataset, some words censored under the
+         “sexual” category were unnecessary. For instance, the song 
+         “Love Yourself” by Justin Bieber edited the word “sleep” under the
+         sexual reference category. Looking at the original lyric, there was no
+         need for censorship. Another unnecessary instance was from the song
+         “Mercy” by Shawn Mendes, and the censored word in the song was “skin.”
+         After looking at the original lyric, censorship was unnecessary,
+         especially under the sexual category. These examples of extreme
+         censorship under the sexual category may have flawed this data.")
+     )
+   ))
+
+chart_3 <- tabPanel(
+  "Words Vs Frequency",
+   sidebarLayout(
+     sidebarPanel(
+       h3("Words VS Frequency"),
+       selectInput(inputId = "frequency_selection",
+                   label = "Choice of Word",
+                   choices = bad_words$badword,
+                   selected = "addict",
+                   multiple = TRUE)),
+     mainPanel(
+       h1("How Often are the Words Censored"),
+       plotlyOutput("frequency_plot")
+     )
+   ))
+
+conclusion <- tabPanel("Conclusion")
+
+ui <- navbarPage("Kidz Bop Censorship",
+                 intro_page,
+                 chart_1,
+                 chart_2,
+                 chart_3,
+                 conclusion
+)
